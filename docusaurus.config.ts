69e7d80c054b4ab721e1e81cefeaf7f6b1cf98ca
xhad <chad@loreum.org>
2025-01-16 17:51:00 +0800

    update the favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'https://cdn.loreum.org/logos/white.svg',
   organizationName: 'Loreum',
   projectName: 'docs',
 
